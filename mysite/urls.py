4d439d814f3aebda4fa35f139e826f3b6a536f8a
ptejashree88 <tejashree.p88@gmail.com>
2021-04-25 15:35:46 +0530

    urls.py commit by Tejashree

--- mysite/urls.py
+++ mysite/urls.py
@@ -19,3 +19,15 @@ from django.urls import path
 urlpatterns = [
     path('admin/', admin.site.urls),
 ]
+
+class Dreamreal(models.Model):
+
+   website = models.CharField(max_length = 50)
+   mail = models.CharField(max_length = 50)
+   website = models.CharField(max_length = 50)
+   mail = models.CharField(max_length = 50)
+   name = models.CharField(max_length = 50)
+   phonenumber = models.IntegerField()
+
+   class Meta:
+      db_table = "dreamreal"
